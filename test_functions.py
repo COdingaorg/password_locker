b3a243a2947e1927030a411b5f227e273c221054
Caleb Odinga <calemasanga@gmail.com>
2021-05-28 22:24:29 +0300

    adds a setup function in the test file

--- test_functions.py
+++ test_functions.py
@@ -2,3 +2,12 @@ import unittest
 from credentialsClass import CredentialsClass
 from userClass import UserClass
 
+class TestClass(unittest.TestCase):
+  '''
+  creates a class for testing functions in the application
+  '''
+  def setUp(self):
+    '''
+    set up functions that runs after each test function
+    '''
+    self.new_user=UserClass('Benard','Kibet','benard.kibet@student.motingaschool.com','password')
