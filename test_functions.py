00be549a82dc6a875546c2185a84ad96dc6664a4
Caleb Odinga <calemasanga@gmail.com>
2021-05-28 22:35:52 +0300

    adds a test_user_creation function to check if a user objects is created

--- test_functions.py
+++ test_functions.py
@@ -10,4 +10,13 @@ class TestClass(unittest.TestCase):
     '''
     set up functions that runs after each test function
     '''
-    self.new_user=UserClass('Benard','Kibet','benard.kibet@student.motingaschool.com','password')
+    self.new_user = UserClass('Benard','Kibet','benard.kibet@student.motingaschool.com','password')
+  
+  def test_user_creation(self):
+    '''
+    checks if a function creates a user
+    '''
+    self.assertEqual(self.new_user.first_name,'Benard')
+    self.assertEqual(self.new_user.last_name,'Kibet')
+    self.assertEqual(self.new_user.email,'benard.kibet@student.motingaschool.com')
+    self.assertEqual(self.new_user.password,'password')
